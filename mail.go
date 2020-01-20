8fe802917b760958e99135f2b42bef68d88636d6
nbxtruong <omega220@gmail.com>
2020-01-21 00:23:56 +0700

    (+) Apply strategy pattern

--- mail.go
+++ mail.go
@@ -1,10 +1,11 @@
 package email
 
-// IMail store function in email package
+// IMail interface for strategy pattern
 type IMail interface {
 	Send(from, recipient, subject, msg string) error
 }
 
+// Mail for context embeds the strategy
 type Mail struct {
 	mailClient IMail
 	from       string
@@ -13,14 +14,25 @@ type Mail struct {
 	msg        string
 }
 
-func InitMail(m IMail, from, recipient, subject, msg string) *Mail {
+func InitMailClient(client IMail, from, recipient, subject, msg string) *Mail {
 	return &Mail{
-		mailClient: m,
+		mailClient: client,
 		from:       from,
 		recipient:  recipient,
 		subject:    subject,
 		msg:        msg,
 	}
+}
+
+func (m *Mail) SetMailClient(client IMail) {
+	m.mailClient = client
+}
+
+func (m *Mail) SetMailContext(from, recipient, subject, msg string) {
+	m.from = from
+	m.recipient = recipient
+	m.subject = subject
+	m.msg = msg
 }
 
 func (m *Mail) Sent() {
